e85547cd391958ed13781161fa46f6895cf5c5cc
lbwright22ci <lbwright22@gmail.com>
2026-06-24 09:44:17 +0100

    wire up a simple home page view and test that urls work correctly: pass

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_page(request):
+    """
+    Renders the home page for the Loopy e-commerce site
+
+    **Template**
+    :template: `home.html`
+
+    """
+    template= 'core/home.html'
+    context = {}
+
+    return render (request, template, context)
