74a9cd4c76a5f0997cb2a4396adc7b5cdac0abd8
lbwright22ci <lbwright22@gmail.com>
2026-06-25 10:03:16 +0100

    add jQuery to open nav sub menus on mouse hover

---
+++ static/js/base.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $(".dropdown").on("mouseover", function(){
+        element = $(this);
+        element.children("ul:first-of-type").addClass("show");
+    });
+
+    $(".dropdown").on("mouseleave", function(){
+        $("ul:first-of-type").removeClass("show");
+    });
+
+
+});
