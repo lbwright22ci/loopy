42630fe93a26491f76abb3cafde929b953490d97
lbwright22ci <lbwright22@gmail.com>
2026-06-26 15:27:23 +0100

    write mobile menu filter yarn as dropdowns. This is not ideal as expanded drop down hides text underneath

--- static/js/base.js
+++ static/js/base.js
@@ -9,40 +9,40 @@ $(document).ready(function(){
         $("ul:first-of-type").removeClass("show");
     });
 
-    $("#mobile-hamburger").on("click", function(){
-        $("#expand-hambuger").toggleClass("show");
-    });
+    // $("#mobile-hamburger").on("click", function(){
+    //     $("#expand-hambuger").toggleClass("show");
+    // });
 
-    $("#user-toggle").on("click", function(){
-        $("#user-toggle-reveal").toggleClass("show");
-        $(this).toggleClass("show");
-        state = $(this).attr('aria-expanded');
-        if (state ==='true'){
-            $(this).attr('aria-expanded','false');
-        } else{
-            $(this).attr('aria-expanded','true');
-        };
-    });
+    // $("#user-toggle").on("click", function(){
+    //     $("#user-toggle-reveal").toggleClass("show");
+    //     $(this).toggleClass("show");
+    //     state = $(this).attr('aria-expanded');
+    //     if (state ==='true'){
+    //         $(this).attr('aria-expanded','false');
+    //     } else{
+    //         $(this).attr('aria-expanded','true');
+    //     };
+    // });
 
-    $(document).mouseup(function(e){
-        if($("#expand-hambuger").hasClass("show")){
-            $("#yarn-menu-mobile").on("click", function(){
-                $('#expand-hambuger').removeClass("show");
-                $('#yarn-mobile-filter').addClass("show");
-            });
-            if($(e.target).closest("#expand-hambuger").length===0){
-                $('#expand-hambuger').removeClass("show");
-            };
-        };
-        if($("#yarn-mobile-filter").hasClass("show")){
-            $("#mob-back-filter").on("click", function(){
-                $('#yarn-mobile-filter').removeClass("show");
-                $('#expand-hambuger').addClass("show");
-            });
-            if($(e.target).closest("#yarn-mobile-filter").length===0){
-                $('#yarn-mobile-filter').removeClass("show");
-            };
-        };
-    });
+    // $(document).mouseup(function(e){
+    //     if($("#expand-hambuger").hasClass("show")){
+    //         $("#yarn-menu-mobile").on("click", function(){
+    //             $('#expand-hambuger').removeClass("show");
+    //             $('#yarn-mobile-filter').addClass("show");
+    //         });
+    //         if($(e.target).closest("#expand-hambuger").length===0){
+    //             $('#expand-hambuger').removeClass("show");
+    //         };
+    //     };
+    //     if($("#yarn-mobile-filter").hasClass("show")){
+    //         $("#mob-back-filter").on("click", function(){
+    //             $('#yarn-mobile-filter').removeClass("show");
+    //             $('#expand-hambuger').addClass("show");
+    //         });
+    //         if($(e.target).closest("#yarn-mobile-filter").length===0){
+    //             $('#yarn-mobile-filter').removeClass("show");
+    //         };
+    //     };
+    // });
 
 });
