de2a1756010c385cbff33041e089fcd66df40ecb
lbwright22ci <lbwright22@gmail.com>
2026-07-07 16:24:29 +0100

    main and mobile menu filtering for products wired up

--- product/views.py
+++ product/views.py
@@ -21,6 +21,7 @@ def AllProducts(request):
     thicknesses = None
     fibres = None
     colours = None
+    natural_yarn= None
 
     sort = None
     direction = None
@@ -39,23 +40,25 @@ def AllProducts(request):
             product_list = product_list.filter(queries)
         if 'brand' in request.GET:
             brands = request.GET['brand']
-            product_list = product_list.filter(brand_id__name__in=brands)
-            brands = Brand.objects.filter(name__in=brands)
+            product_list = product_list.filter(brand_id__name__icontains=brands)
         if 'thickness' in request.GET:
             thicknesses = request.GET['thickness']
             product_list = product_list.filter(thickness_id__name__icontains=thicknesses)
-            thicknesses = Thickness.objects.filter(name__in=thicknesses)
+            
         if 'fibre' in request.GET:
-            fibres = request.GET['fibre'].split(',')
-            product_list = product_list.filter(fibre__in=fibres)
-            fibres = Product.objects.filter(fibre__icontains=fibres)
+            fibres = request.GET['fibre']
+            product_list = product_list.filter(fibre__icontains=fibres)
+            
         if 'colour' in request.GET:
             colours = request.GET['colour'].split(',')
             product_list = set(product_list.filter(product__colour_cat_id__shade_type_id__name__in=colours))
-            colours = Shade_Type.objects.filter(name__in=colours)
+           
         if 'sale' in request.GET:
             sales = True
             product_list = product_list.filter(on_promotion=True)
+        if 'natural' in request.GET:
+            natural_yarn = True
+            product_list= product_list.filter(natural_fibres=True)
 
     context={
         'product_list':product_list,
@@ -65,6 +68,8 @@ def AllProducts(request):
         'current_fibres':fibres,
         'current_colours':colours,
         'sales':sales,
+        'natural':natural_yarn,
+        'current_query':query,
     }
     template = 'product/all-products.html'
 
