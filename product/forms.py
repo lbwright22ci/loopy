7b13758dcb025234c301561bdc8e0352484d5002
lbwright22ci <lbwright22@gmail.com>
2026-07-10 13:24:14 +0100

    build framework of product detail page giving essential information. Does not include colour options. Not styled optimally

--- product/forms.py
+++ product/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import Colour_cat
+from .models import Colour_var
 
 class ColourVarForm(forms.ModelForm):
     """ """
     class Meta:
-        model= Colour_cat
-        fields=['colour_name']
+        model= Colour_var
+        fields=['colour_cat_id']
