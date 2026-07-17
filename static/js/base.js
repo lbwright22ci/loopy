d7327769aeb46a417f6c37c961e75f8800dd59af
lbwright22ci <lbwright22@gmail.com>
2026-07-17 12:05:11 +0100

    fix x overflow in footer styling across the site. basket preview opens on latop only. Does not open on mobile and tablet

--- static/js/base.js
+++ static/js/base.js
@@ -16,9 +16,22 @@ $(document).ready(function(){
         $('#show-basket').toggleClass('d-none');
     });
 
+    $('#basket-mob').click(function(e){
+        e.preventDefault();
+        $('#show-basket').toggleClass('d-none');
+    });
+
     $('#show-basket').on('mouseleave', function(e){
         e.preventDefault();
         $(this).toggleClass('d-none');
+    });
+
+    $(document).mouseup(function(e){
+        if(!$("#show-basket").hasClass("d-none")){
+            if($(e.target).closest("#show-basket").length===0){
+                $('#show-basket').addClass("d-none");
+            };
+        };
     });
 
     $('.toast').toast('show');
