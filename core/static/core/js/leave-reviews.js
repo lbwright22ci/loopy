e84fe0c532fcca5bb2983c35fbf1e3c8e4012839
lbwright22ci <lbwright22@gmail.com>
2026-08-04 17:26:40 +0100

    build initial stages o star rating jquery input for leave feedback form.  Doesn't output the reading

--- core/static/core/js/leave-reviews.js
+++ core/static/core/js/leave-reviews.js
@@ -6,6 +6,34 @@ $(document).ready(function () {
     $('textarea').eq(i).val(cc);
   }
 
+  $('.1').click(function(){
+    $(this).nextAll().removeClass('fa-solid').addClass('fa-regular');
+    $(this).removeClass('fa-regular').addClass('fa-solid');
+    $(this).next("input[name='rating']").attr('value', 1);
+  });
+  $('.2').click(function(){
+    $(this).nextAll().removeClass('fa-solid').addClass('fa-regular');
+    $(this).removeClass('fa-regular').addClass('fa-solid');
+    $(this).prevAll().removeClass('fa-regular').addClass('fa-solid');
+    $(this).closest("input[name='rating']").attr('value', 2);
+  });
+  $('.3').click(function(){
+    $(this).nextAll().removeClass('fa-solid').addClass('fa-regular');
+    $(this).removeClass('fa-regular').addClass('fa-solid');
+    $(this).prevAll().removeClass('fa-regular').addClass('fa-solid');
+    $(this).closest("input[name='rating']").attr('value', 3);
+  });
+  $('.4').click(function(){
+    $(this).nextAll().removeClass('fa-solid').addClass('fa-regular');
+    $(this).removeClass('fa-regular').addClass('fa-solid');
+    $(this).prevAll().removeClass('fa-regular').addClass('fa-solid');
+    $(this).closest("input[name='rating']").attr('value', 4);
+  });
+  $('.5').click(function(){
+    $(this).removeClass('fa-regular').addClass('fa-solid');
+    $(this).prevAll().removeClass('fa-regular').addClass('fa-solid');
+    $(this).closest("input[name='rating']").attr('value', 5);
+  });
 
   $('.feedback').click(function(e){
         var form = $(this).closest('.feedback-form');
