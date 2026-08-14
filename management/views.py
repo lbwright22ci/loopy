d082c24ee474a67f123b60d3716e13537b629f6b
lbwright22ci <lbwright22@gmail.com>
2026-08-14 13:22:45 +0100

    start to build html for shop order admin page with responsive styling

--- management/views.py
+++ management/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,  redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 
 from .forms import ShopContactInfoForm
 from core.models import SaleSettings, ShopContactInfo, Announcements
-
+from checkout.models import Order
 
 # Create your views here.
 
@@ -17,7 +18,6 @@ def management_home(request):
         return redirect(reverse('home'))
 
     context={
-
     }
     template = 'management/admin-home.html'
 
@@ -145,8 +145,22 @@ def management_orders(request):
                              Loopy Yarns staff.")
         return redirect(reverse('home'))
 
-    context={
+    pending_queryset = Q(is_shipped = False) & Q(refund_status = False)
+    pending = Order.objects.filter(pending_queryset)
 
+    cancelled_queryset = Q(is_shipped = False) & Q(refund_status = True)
+    cancelled = Order.objects.filter(cancelled_queryset)
+
+    refunded_queryset = Q(refund_status = True) & Q(is_shipped= True)
+    refunded = Order.objects.filter(refunded_queryset)
+
+    past = Order.objects.exclude(pending_queryset, cancelled_queryset, refunded_queryset)
+
+    context={
+        'pending':pending,
+        'cancelled':cancelled,
+        'refunded':refunded,
+        'past':past,
     }
     template = 'management/admin-orders.html'
 
