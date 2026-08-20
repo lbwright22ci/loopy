4e66fd73973e70ee9dd3e214e240896f2a488124
lbwright22ci <lbwright22@gmail.com>
2026-08-20 10:40:09 +0100

    add robots.txt file

--- loopy/urls.py
+++ loopy/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
@@ -28,4 +29,7 @@ urlpatterns = [
     path('management/', include('management.urls')),
     path('yarns/', include('product.urls')),
     path('', include('core.urls')),
+    path('robots.txt',
+         TemplateView.as_view(template_name=robots.txt,
+                              content_type="text/plain"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
