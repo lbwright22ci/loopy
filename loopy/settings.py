e455fb2bbaa77f82e0561baf0d1484eff177d0af
lbwright22ci <lbwright22@gmail.com>
2026-09-01 16:21:30 +0100

    finish documenting testing and deployment

--- loopy/settings.py
+++ loopy/settings.py
@@ -188,7 +188,6 @@ if "USE_AWS" in os.environ:
     AWS_S3_REGION_NAME = "us-east-1"
     AWS_STORAGE_BUCKET_NAME = "loopy-yarns-uk"
     AWS_S3_FILE_OVERWRITE = False
-
     AWS_DEFAULT_ACL = 'public-read'
 
     AWS_QUERYSTRING_AUTH = True
@@ -220,17 +219,18 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Email
 
-# if 'DEVELOPMENT' in os.environ:
-#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#     DEFAULT_FROM_EMAIL='loopyyarnsuk@gmail.com'
-# else:
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_APP_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL='loopyyarnsuk@gmail.com'
+else:
+
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_APP_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # AllAuth
 
